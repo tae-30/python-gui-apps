37e5089713e604adc58ac16ed821e0409010e874
tae-30 <yuut0302@icloud.com>
2024-07-22 15:49:46 +0900

    commit

---
+++ Monthcalender.project/kakeibo.py
@@ -0,0 +1,200 @@
+import tkinter as tk
+from datetime import datetime
+
+
+window = tk.Tk()
+window.title("GUI App")
+window.geometry("600x400")
+bg_color = "#333333"
+fg_color = "white"
+highlight_color = "white"
+
+row_cou = 3
+today = datetime.today()
+
+
+# エンターが押されたときに新しい入力欄を作成し計算する処理
+def enter_action(event):
+    global row_cou
+    try:
+        money = int(money_entry.get())
+        money2 = int(money_entry2.get())
+        money_calc = money - money2
+        money_entry.delete(0, tk.END)
+        money_entry.insert(0, str(money_calc))
+    # エラーが起きた場合、passする
+    except ValueError:
+        pass
+    # 商品名を保存
+    new_label = tk.Label(
+        frame,
+        text="",
+        bg=bg_color,
+        fg=fg_color,
+        font=("Times", 20),
+        highlightbackground=highlight_color,
+        highlightthickness=1,
+    )
+    new_label.grid(row=row_cou, column=1)
+    new_label.config(text=name_entry2.get())
+    # 金額を保存
+    new_label2 = tk.Label(
+        frame,
+        text="",
+        bg=bg_color,
+        fg=fg_color,
+        font=("Times", 20),
+        highlightbackground=highlight_color,
+        highlightthickness=1,
+    )
+    new_label2.grid(row=row_cou, column=2)
+    new_label2.config(text=money_entry2.get())
+    # 日付を保存
+    new_label3 = tk.Label(
+        frame,
+        text=today.strftime("%m-%d"),
+        bg=bg_color,
+        fg=fg_color,
+        font=("Times", 20),
+        highlightbackground=highlight_color,
+        highlightthickness=1,
+    )
+    new_label3.grid(row=row_cou, column=0)
+    # day_get = day_label.get()
+    # new_label3.config(text=day_get)
+
+    # day_entry.delete(0, tk.END)
+    name_entry2.delete(0, tk.END)
+    money_entry2.delete(0, tk.END)
+
+    row_cou += 1
+    name_entry2.focus_set()
+
+
+# 右矢印が押された時のカーソルの動き
+def right_action(event):
+    focus = window.focus_get()
+
+    if focus == name_entry2:
+        money_entry2.focus_set()
+    # elif focus == money_entry2:
+    #     pass
+
+
+# 左矢印が押された時のカーソルの動き
+def left_action(event):
+    focus = window.focus_get()
+
+    if focus == money_entry2:
+        name_entry2.focus_set()
+    # elif focus == name_entry2:
+    #     pass
+
+
+# 上矢印が押された時のカーソルの動き
+def up_action(event):
+    focus = window.focus_get()
+
+    if focus == money_entry2 or focus == name_entry2:
+        money_entry.focus_set()
+    elif focus == money_entry:
+        name_entry2.focus_set()
+
+
+# 下矢印が押された時のカーソルの動き
+def down_action(event):
+    focus = window.focus_get()
+
+    if focus == money_entry:
+        name_entry2.focus_set()
+    elif focus == name_entry2:
+        money_entry.focus_set()
+
+
+label1 = tk.Label(window, text="家計簿", bg=bg_color, fg=fg_color, font=("Times", 40))
+label1.pack(pady=10)
+
+frame = tk.Frame(window, bg=bg_color)
+frame.pack(pady=10)
+
+
+label2 = tk.Label(frame, text="残り", bg=bg_color, fg=fg_color, font=("Times", 25))
+label2.grid(row=0, column=0)
+
+# 残りの金額を表示する欄
+money_entry = tk.Entry(
+    frame,
+    text="",
+    bg=bg_color,
+    fg=fg_color,
+    font=("Times", 25),
+    highlightbackground=highlight_color,
+    highlightthickness=1,
+)
+money_entry.grid(row=0, column=1)
+money_entry.bind("<Up>", up_action)
+money_entry.bind("<Down>", down_action)
+
+label3 = tk.Label(frame, text="円", bg=bg_color, fg=fg_color, font=("Times", 25))
+label3.grid(row=0, column=2, sticky="w")
+
+day = tk.Label(frame, text="日付", bg=bg_color, fg=fg_color, font=("Times", 20))
+day.grid(row=1, column=0)
+
+buy_label = tk.Label(frame, text="用途", bg=bg_color, fg=fg_color, font=("Times", 20))
+buy_label.grid(row=1, column=1)
+
+money_label = tk.Label(frame, text="金額", bg=bg_color, fg=fg_color, font=("Times", 20))
+money_label.grid(row=1, column=2)
+
+
+# 日付を表示
+day_label = tk.Label(
+    frame,
+    text=datetime.today().strftime("%m-%d"),
+    bg=bg_color,
+    fg=fg_color,
+    font=("Times", 20),
+    width=5,
+    highlightbackground=highlight_color,
+    highlightthickness=1,
+)
+day_label.grid(row=2, column=0, sticky="w")
+
+# 商品名を入力する欄
+name_entry2 = tk.Entry(
+    frame,
+    text="",
+    bg=bg_color,
+    fg=fg_color,
+    font=("Times", 20),
+    width=15,
+    highlightbackground=highlight_color,
+    highlightthickness=1,
+)
+name_entry2.grid(row=2, column=1)
+name_entry2.bind("<Return>", enter_action)
+name_entry2.bind("<Right>", right_action)
+name_entry2.bind("<Left>", left_action)
+name_entry2.bind("<Up>", up_action)
+name_entry2.bind("<Down>", down_action)
+
+# 金額を入力する欄
+money_entry2 = tk.Entry(
+    frame,
+    text="",
+    bg=bg_color,
+    fg=fg_color,
+    font=("Times", 20),
+    width=15,
+    highlightbackground=highlight_color,
+    highlightthickness=1,
+)
+money_entry2.grid(row=2, column=2)
+money_entry2.bind("<Return>", enter_action)
+money_entry2.bind("<Right>", right_action)
+money_entry2.bind("<Left>", left_action)
+money_entry2.bind("<Up>", up_action)
+money_entry2.bind("<Down>", down_action)
+
+window.mainloop()
--- xwork6_2.py
+++ xwork6_2.py
@@ -4,34 +4,42 @@ import random
 # お約束のコード
 window = tk.Tk()
 window.title("GUI App")
-window.geometry("600x400")
+window.geometry("600x500")
 bg_color = "#333333"  # ダークグレー
 fg_color = "#FFFFFF"  # 白
 # bg_color2 = "#FF0000"
 
 window.configure(bg=bg_color)
 
+maru_batu = 0
+
 
 # ボタンが押された時の処理
-class Maru:
-    def button_action(self, i, j):
-        if buttons[i][j].cget("text") == "":
+def button_action(i, j):
+    global maru_batu
+    if buttons[i][j].cget("text") == "":
+        if maru_batu == 0:
             buttons[i][j].config(text="◯")
-            # buttons[i][j].config(state="disabled")
-            if check_win_maru():
-                label2.config(text="あなたの勝ちです")
-
-
-class Batu:
-    def button_action(self, i, j):
-        if buttons[i][j].cget("text") == "":
+            maru_batu = 1
+            if maru_batu == 1:
+                cpu_action()
+        elif maru_batu == 1:
             buttons[i][j].config(text="✖️")
-            if check_win_batu():
-                label2.config(text="コンピュータの勝ちです")
+            maru_batu = 0
+    if check_win("◯"):
+        label2.config(text="あなたの勝ちです")
+    elif check_draw():
+        label2.config(text="引き分けです")
 
 
 # コンピュータの動きを処理する関数
 def cpu_action():
+    global maru_batu
+    # 真ん中が空いている場合真ん中を取る
+    if buttons[1][1].cget("text") == "":
+        buttons[1][1].config(text="✖️")
+        maru_batu = 0
+        return
     # 勝利できるマスを探す
     for i in range(3):
         for j in range(3):
@@ -39,7 +47,7 @@ def cpu_action():
                 # 全ての空白にバツを置いてみる
                 buttons[i][j].config(text="✖️")
                 # もし勝利条件を満たした場合
-                if check_win_batu():
+                if check_win("✖️"):
                     label2.config(text="コンピュータの勝ちです")
                     return  # 関数の処理を終了する
                 buttons[i][j].config(text="")
@@ -51,9 +59,10 @@ def cpu_action():
                 # 全ての空白にマルを置いてみる
                 buttons[i][j].config(text="◯")
                 # もしマルの勝利条件を満たした場合
-                if check_win_maru():
+                if check_win("◯"):
                     # バツを置いて阻止する
                     buttons[i][j].config(text="✖️")
+                    maru_batu = 0
                     return  # 終了
                 buttons[i][j].config(text="")
 
@@ -67,31 +76,12 @@ def cpu_action():
     if random_action:
         i, j = random.choice(random_action)
         buttons[i][j].config(text="✖️")
-
-
-maru_batu = 0
-
-
-# ボタンが押された時の処理を行う関数
-def button_action(i, j):
-    global maru_batu
-    if maru_batu == 0:
-        maru_instance = Maru()
-        maru_instance.button_action(i, j)
-        maru_batu = 1
-        # もしマルの勝利条件を満たさなかった場合
-        if not check_win_maru():
-            # cpuを呼び出す
-            cpu_action()
-            maru_batu = 0
-    elif maru_batu == 1:
-        batu_instance = Batu()
-        batu_instance.button_action(i, j)
         maru_batu = 0
 
 
 # リセットボタンが押された時の処理を行う関数
 def reset_action():
+    global maru_batu
     for row in buttons:
         for button in row:
             button.config(text="")
@@ -101,7 +91,9 @@ def reset_action():
 
 
 # ラベルを設置
-label1 = tk.Label(window, text="あなたは◯です", bg=bg_color, fg=fg_color)
+label1 = tk.Label(
+    window, text="あなたは◯です", bg=bg_color, fg=fg_color, font=("Helvetica", 25)
+)
 label1.pack(pady=10)
 
 # リセットボタンを設置
@@ -130,6 +122,8 @@ for i in range(3):
             command=lambda r=i, c=j: button_action(r, c),
             bg=fg_color,
             fg=bg_color,
+            width=2,
+            height=2,
             font=("Helvetica", 25),
         )
         button.grid(row=i, column=j, padx=10, pady=10)
@@ -137,77 +131,52 @@ for i in range(3):
     buttons.append(row_button)
 
 
-# 勝利判定を行う関数（◯が揃った場合）
-def check_win_maru():
+# 勝利判定を行う関数
+def check_win(a):
     for row in buttons:
         if (
-            row[0].cget("text") == "◯"
-            and row[1].cget("text") == "◯"
-            and row[2].cget("text") == "◯"
+            row[0].cget("text") == a
+            and row[1].cget("text") == a
+            and row[2].cget("text") == a
         ):
             return True
 
     for col in range(3):
         if (
-            buttons[0][col].cget("text") == "◯"
-            and buttons[1][col].cget("text") == "◯"
-            and buttons[2][col].cget("text") == "◯"
+            buttons[0][col].cget("text") == a
+            and buttons[1][col].cget("text") == a
+            and buttons[2][col].cget("text") == a
         ):
             return True
 
     if (
-        buttons[0][0].cget("text") == "◯"
-        and buttons[1][1].cget("text") == "◯"
-        and buttons[2][2].cget("text") == "◯"
+        buttons[0][0].cget("text") == a
+        and buttons[1][1].cget("text") == a
+        and buttons[2][2].cget("text") == a
     ):
         return True
 
     if (
-        buttons[2][0].cget("text") == "◯"
-        and buttons[1][1].cget("text") == "◯"
-        and buttons[0][2].cget("text") == "◯"
+        buttons[2][0].cget("text") == a
+        and buttons[1][1].cget("text") == a
+        and buttons[0][2].cget("text") == a
     ):
         return True
 
     return False
 
 
-# 勝利判定を行う関数（✖️が揃った場合）
-def check_win_batu():
+# 引き分け判定を行う関数
+def check_draw():
     for row in buttons:
-        if (
-            row[0].cget("text") == "✖️"
-            and row[1].cget("text") == "✖️"
-            and row[2].cget("text") == "✖️"
-        ):
-            return True
-
-    for col in range(3):
-        if (
-            buttons[0][col].cget("text") == "✖️"
-            and buttons[1][col].cget("text") == "✖️"
-            and buttons[2][col].cget("text") == "✖️"
-        ):
-            return True
-
-    if (
-        buttons[0][0].cget("text") == "✖️"
-        and buttons[1][1].cget("text") == "✖️"
-        and buttons[2][2].cget("text") == "✖️"
-    ):
-        return True
-
-    if (
-        buttons[2][0].cget("text") == "✖️"
-        and buttons[1][1].cget("text") == "✖️"
-        and buttons[0][2].cget("text") == "✖️"
-    ):
-        return True
-
-    return False
+        for button in row:
+            # ボタンに空白が残っている場合Falseを返す
+            if button.cget("text") == "":
+                return False
+    # 空白がない場合Trueを返す
+    return True
 
 
-# お約束のコード
 label2 = tk.Label(window, text="", bg=bg_color, fg=fg_color, font=("Helvetica", 25))
 label2.pack(pady=10)
 
